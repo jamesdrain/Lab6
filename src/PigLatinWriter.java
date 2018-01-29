e2c2d8e0c4e9b35f79544e72d64287cf88c19859
jamesdrain <james.drain@yahoo.com>
2018-01-29 09:44:42 -0500

    shortened word modification methods

--- src/PigLatinWriter.java
+++ src/PigLatinWriter.java
@@ -13,24 +13,29 @@ public class PigLatinWriter {
 		
 		while (cont.equalsIgnoreCase("y")) {
 			
+			// Get user input 
+			
 			System.out.println("Enter a word: ");
 			userWord = scnr.nextLine();
 			userWord = userWord.toLowerCase();
+			
+			// Check for vowel in first char and then consonants at 1st 2nd 3rd letter
+			
 			if (userWord.charAt(0) == 'a' || userWord.charAt(0) == 'e' || userWord.charAt(0) == 'i' || 
 					userWord.charAt(0) == 'o' || userWord.charAt(0) == 'u') {
 				pigWord = userWord + ("way");
 				System.out.println(pigWord);
 			} else if (userWord.charAt(1) == 'a' || userWord.charAt(1) == 'e' || userWord.charAt(1) == 'i' || 
 					userWord.charAt(1) == 'o' || userWord.charAt(1) == 'u') {
-				System.out.println(modifyOneCons(userWord));
+				System.out.println(modifyCons(userWord, 1));
 				
 			} else if (userWord.charAt(2) == 'a' || userWord.charAt(2) == 'e' || userWord.charAt(2) == 'i' || 
 					userWord.charAt(2) == 'o' || userWord.charAt(2) == 'u') {
-				System.out.println(modifyTwoCons(userWord));
+				System.out.println(modifyCons(userWord, 2));
 				
 			} else if (userWord.charAt(3) == 'a' || userWord.charAt(3) == 'e' || userWord.charAt(3) == 'i' || 
 					userWord.charAt(3) == 'o' || userWord.charAt(3) == 'u') {
-				System.out.println(modifyThreeCons(userWord));
+				System.out.println(modifyCons(userWord, 3));
 				
 			} else {
 				System.out.println("Wow, that's a weird word...");
@@ -42,28 +47,17 @@ public class PigLatinWriter {
 		}
 		System.out.println("Ok, bye. ");
 		scnr.close();
+		
+		// Word modification methods
 	}
-	public static String modifyOneCons(String userWord) {
-		String modWord1 = userWord.substring(1);
-		String modWord2 = userWord.substring(0, 1);
+	public static String modifyCons(String userWord, int cons) {
+		String modWord1 = userWord.substring(cons);
+		String modWord2 = userWord.substring(0, cons);
 		String pigWord = (modWord1 + modWord2 + "ay");
 		
 		return pigWord;
 	}
-	public static String modifyTwoCons(String userWord) {
-		String modWord1 = userWord.substring(2);
-		String modWord2 = userWord.substring(0, 2);
-		String pigWord = (modWord1 + modWord2 + "ay");
-		
-		return pigWord;
-	}
-	public static String modifyThreeCons(String userWord) {
-		String modWord1 = userWord.substring(3);
-		String modWord2 = userWord.substring(0, 3);
-		String pigWord = (modWord1 + modWord2 + "ay");
-		
-		return pigWord;
-	}
+	
 	
 }
 
